9a355df59160a6f1a95920e429ace652219cbe8b
NhanNee <hihi@gmail.com>
2019-09-26 17:44:13 +0700

    TestIsOdd

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -6,11 +6,19 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class Program
+   public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine(" Hello Github");
         }
+
+        public static bool IsOdd( int n )
+        {
+            if (n % 2 == 0)
+                return false;
+            return true;
+        }
+
     }
 }
