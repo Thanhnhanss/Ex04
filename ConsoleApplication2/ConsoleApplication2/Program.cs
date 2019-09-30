eec44e9a810c9954b61924e16a6251bda864b736
Thanhnhanss <@gmail.com>
2019-09-30 21:46:12 +0700

    iseven

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+         
+        }
+        public static bool IsEven(int n)
+        {
+            if (n % 2 == 0)
+                return true;
+            return false;
+        }
+    }
+}
---
+++ ConsoleApplication2/UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication2;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void IsEven()
+        {
+            bool result5 = Program.IsEven(5);
+            Assert.AreEqual(false, result5);
+
+            bool result6 = Program.IsEven(6);
+            Assert.AreEqual(true, result6);
+        }
+    }
+}
